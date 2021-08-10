2684790aaca6ff373898e3d2028c4080d816bbc1
Rishabh Kunwar <rishabhkunwar11@gmail.com>
2021-08-10 12:26:51 +0530

    feat : added sign up form

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,128 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Visibility, VisibilityOff } from "@material-ui/icons";
+import "../styles/login.css";
 
 export const Signup = () => {
-  return <div>Sign up Page</div>;
+  const [userInput, setUserInput] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState({
+    firstNameError: "",
+    lastNameError: "",
+    emailError: "",
+    passwordError: "",
+    confirmPasswordError: "",
+  });
+  const signUpHandler = () => {};
+  return (
+    <form onSubmit={signUpHandler} className="login signup">
+      <h1>Sign Up</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          value={userInput.firstName}
+          placeholder="Enter your First Name"
+          onChange={(e) =>
+            setUserInput((prev) => ({ ...prev, firstName: e.target.value }))
+          }
+          className="inputText"
+        />
+        <small className="error">{error.firstNameError}</small>
+      </div>
+      <div className="input-container">
+        <input
+          type="text"
+          value={userInput.lastName}
+          placeholder="Enter your Last Name"
+          onChange={(e) =>
+            setUserInput((prev) => ({ ...prev, lastName: e.target.value }))
+          }
+          className="inputText"
+        />
+        <small className="error">{error.lastNameError}</small>
+      </div>
+      <div className="input-container">
+        <input
+          type="text"
+          value={userInput.email}
+          placeholder="Enter your Email"
+          onChange={(e) =>
+            setUserInput((prev) => ({ ...prev, email: e.target.value }))
+          }
+          className="inputText"
+        />
+        <small className="error">{error.emailError}</small>
+      </div>
+      <div className="password">
+        <div className="input-container">
+          <input
+            type={showPassword ? "text" : "password"}
+            value={userInput.password}
+            placeholder="Enter your Password"
+            onChange={(e) =>
+              setUserInput((prev) => ({ ...prev, password: e.target.value }))
+            }
+            className="inputText"
+          />
+          <small className="error">{error.passwordError}</small>
+        </div>
+        <div
+          style={{ width: "fit-content", padding: "0.5rem" }}
+          onClick={() => {
+            setShowPassword((prev) => !prev);
+          }}
+        >
+          {showPassword ? <VisibilityOff /> : <Visibility />}
+        </div>
+      </div>
+      <div className="password">
+        <div className="input-container">
+          <input
+            type={showPassword ? "text" : "password"}
+            value={userInput.confirmPassword}
+            placeholder="Re Enter your Password"
+            onChange={(e) =>
+              setUserInput((prev) => ({
+                ...prev,
+                confirmPassword: e.target.value,
+              }))
+            }
+            className="inputText"
+          />
+          <small className="error">{error.confirmPasswordError}</small>
+        </div>
+        <div
+          style={{ width: "fit-content", padding: "0.5rem" }}
+          onClick={() => {
+            setShowPassword((prev) => !prev);
+          }}
+        >
+          {showPassword ? <VisibilityOff /> : <Visibility />}
+        </div>
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Sign Up
+      </button>
+
+      <small style={{ textAlign: "center", margin: "1rem 0rem" }}>
+        allready have an account ,{" "}
+        <Link to="/login">
+          <span
+            style={{
+              textDecoration: "underline",
+              cursor: "pointer",
+            }}
+          >
+            Login Here
+          </span>{" "}
+        </Link>
+      </small>
+    </form>
+  );
 };
